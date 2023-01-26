8e69aa3947aafeb247b424ece31c118bdbe91a07
Ashish Jha <ashishjha14411@gmail.com>
2023-01-26 14:55:54 +0530

    added the product card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import producList from '../data/product.json'
+import React, {useContext} from 'react'
+import productList from '../data/product.json'
+import { CartContext } from '../context/CartContext';
 const Card = () => {
+    
+const {addToCart} = useContext(CartContext)
   return (
-    <div>Card</div>
+    <div className="Card">
+    {productList && productList.map((item) => {
+      return (
+        <div key={item.id}>
+          <img src={item.imgUrl} alt="product-img" />
+          <p className='title'>{item.name}</p>
+          <p className='pricetag'>Price: {item.price}</p>
+          <button className='addbtn'
+          onClick={() => {addToCart(item)}}
+          >Add to Cart</button>
+        </div>
+      );
+    })}
+  </div>
   )
 }
 
