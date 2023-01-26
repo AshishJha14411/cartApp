e0289ae2708466ebf7f5d6d67ac045c376af2f81
Ashish Jha <ashishjha14411@gmail.com>
2023-01-26 15:58:43 +0530

    added the cart with increse,decrese and remove functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
-const Cart = () => {
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+const Cart = ({}) => {
+  const { cartItems,  decreaseFromCart,
+    removeFromCart,addToCart } = useContext(CartContext);
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="Cart">
+      {cartItems &&
+        cartItems.map((item) => {
+          return (
+            <div key={item.id} className="Cart-items">
+              <h1>{item.name}</h1>
+              <p className="price">{item.price}</p>
+              <div className="quantity">
+                <button className="minus" onClick={() => decreaseFromCart(item.id)}>-</button>
+                <p className="quanData">{item.quantity}</p>
+                <button className="plus" onClick={() => {addToCart(item)}}>+</button>
+              </div>
+              <p className="price">{item.price * item.quantity}</p>
+              <button className="removeBtn" onClick={() => removeFromCart(item.id)}>Remove From Cart</button>
+            </div>
+          );
+        })}
+      <p className="total">
+        {`Total: ${cartItems.reduce((total, cartItem) => {
+          return total + cartItem.price * cartItem.quantity;
+        }, 0)}`}
+      </p>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
